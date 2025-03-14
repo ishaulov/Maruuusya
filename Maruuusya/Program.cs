2672a974ee1f6c19a9519651b9a4013a41a91714
Ilya Shaulov <shaulov@macroscop.com>
2025-03-14 12:53:24 +0500

    цикл foreach

--- Maruuusya/Program.cs
+++ Maruuusya/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		List<Test> testsToRun = GetTests();
+
+		foreach (Test test in testsToRun)
+		{
+			var msg = test.Run();
+			if (msg)
+			{
+				Console.WriteLine(msg);
+			}
+		}
+
+		/*for (int i = 0; i < testsToRun.Count; i++)
+		{
+			var msg = Test.Run();
+			if (msg)
+			{
+				Console.WriteLine(msg);
+			}
+
+		}*/
+
+		static List<Test> GetTests()
+		{
+			var testsCode = "1 1 1 0 0 0 1 0 1 0 1".Split(' ');
+			var tests = new List<Test>();
+
+			foreach (var testCode in testsCode)
+			{
+				var test = new Test();
+				test.TestCode = testCode;
+				tests.Add(test);
+			}
+
+			return tests;
+		}
+	}
+
+	class Test
+	{
+		public string TestCode = string.Empty;
+
+		public bool Run()
+		{
+			return TestCode == "1" ? true : false;
+		}
 	}
 }

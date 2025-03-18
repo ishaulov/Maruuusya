bafbd96499dadd2c82ae0b20dc9f7edafbc33e54
Ilya Shaulov <41745869+ishaulov@users.noreply.github.com>
2025-03-18 20:06:52 +0500

    something good

--- Maruuusya/Program.cs
+++ Maruuusya/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
 	static void Main(string[] args)
 	{
+		string nginxConfigPath = @"/etc/nginx/nginx.conf";
+		Console.WriteLine(nginxConfigPath);
+
+		bool happy = true;
+		Console.WriteLine(happy);
+		
+		object height = 1.60; 
+		object name = "Christopher Robin"; 
+		Console.WriteLine($"{name} is {height} metres tall!");
+		// int length1 = name.Length;
+		int length2 = ((string)name).Length; 
+		Console.WriteLine($"{name} has {length2} characters!");
+		
+		dynamic something = "Good";
+		Console.WriteLine($"Good length is {something.Length}");
+		
 		List<Test> testsToRun = GetTests();
 
 		for (int i = 0; i < testsToRun.Count; i++)
@@ -11,11 +27,11 @@ class Program
 			var msg = testsToRun[i].Run();
 			if (msg)
 			{
-				Console.WriteLine($"Номер теста: {i}, успешен!");
+				//Console.WriteLine($"Номер теста: {i}, успешен!");
 			}
 			else
 			{
-				Console.WriteLine($"Номер теста: {i}, провален!");
+				//Console.WriteLine($"Номер теста: {i}, провален!");
 			}
 		}
 

ec070ec198262b1dc25b04a954c9d1cfe4bd525e
Ilya Shaulov <41745869+ishaulov@users.noreply.github.com>
2025-03-25 20:35:28 +0500

    first homework

--- Maruuusya/Program.cs
+++ Maruuusya/Program.cs
@@ -2,62 +2,46 @@
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		string nginxConfigPath = @"/etc/nginx/nginx.conf";
-		Console.WriteLine(nginxConfigPath);
+    static void Main(string[] args)
+    {
+        List<Test> testsToRun = GetTests();
 
-		bool happy = true;
-		Console.WriteLine(happy);
-		
-		object height = 1.60; 
-		object name = "Christopher Robin"; 
-		Console.WriteLine($"{name} is {height} metres tall!");
-		// int length1 = name.Length;
-		int length2 = ((string)name).Length; 
-		Console.WriteLine($"{name} has {length2} characters!");
-		
-		dynamic something = "Good";
-		Console.WriteLine($"Good length is {something.Length}");
-		
-		List<Test> testsToRun = GetTests();
+        for (int i = 0; i < testsToRun.Count; i++)
+        {
+            var msg = testsToRun[i].Run();
+            if (msg)
+            {
+                Console.WriteLine($"Номер теста: {i}, успешен!");
+            }
+            else
+            {
+                Console.WriteLine($"Номер теста: {i}, провален!");
+            }
+        }
 
-		for (int i = 0; i < testsToRun.Count; i++)
-		{
-			var msg = testsToRun[i].Run();
-			if (msg)
-			{
-				//Console.WriteLine($"Номер теста: {i}, успешен!");
-			}
-			else
-			{
-				//Console.WriteLine($"Номер теста: {i}, провален!");
-			}
-		}
+        static List<Test> GetTests()
+        {
+            var testsCode = "1 1 1 0 0 0 1 0 1 0 1".Split(' ');
+            var tests = new List<Test>();
 
-		static List<Test> GetTests()
-		{
-			var testsCode = "1 1 1 0 0 0 1 0 1 0 1".Split(' ');
-			var tests = new List<Test>();
+            foreach (var testCode in testsCode)
+            {
+                var test = new Test();
+                test.TestCode = testCode;
+                tests.Add(test);
+            }
 
-			foreach (var testCode in testsCode)
-			{
-				var test = new Test();
-				test.TestCode = testCode;
-				tests.Add(test);
-			}
+            return tests;
+        }
+    }
 
-			return tests;
-		}
-	}
+    class Test
+    {
+        public string TestCode = string.Empty;
 
-	class Test
-	{
-		public string TestCode = string.Empty;
-
-		public bool Run()
-		{
-			return TestCode == "1" ? true : false;
-		}
-	}
+        public bool Run()
+        {
+            return TestCode == "1" ? true : false;
+        }
+    }
 }
